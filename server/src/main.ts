70cc20a5c23846e21790bafcb5123008e2ec3bf6
Janelle <janellelacsamana@gmail.com>
2023-12-25 21:55:12 +0800

    feature: add error validation messages to response

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,18 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors();
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
 
-  const config = new DocumentBuilder()
-    .setTitle('TigerTiger POS')
-    .setVersion('1.0')
-    .addTag('api')
-    .build();
+  const config = new DocumentBuilder().setTitle('TigerTiger POS').setVersion('1.0').addTag('api').build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
